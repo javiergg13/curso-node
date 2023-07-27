6eff7bd70c36f7706ba4199b7432698beece7e8a
javiergg13 <guerrerogallojavier@gmail.com>
2023-07-27 10:59:24 +0200

    fin primera clase

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const pc = require('picocolors')
 
 const folder = process.argv[2] ?? '.'
 
@@ -8,9 +9,9 @@ async function ls(folder) {
   try {
     files = await fs.readdir(folder)
   } catch (err) {
-    console.error(`Error al leer el directorio ${folder}`)
+    console.error(pc.red(`Error al leer el directorio ${folder}`))
     process.exit(1)
-  }  
+  }
 
   const filesPromises = files.map(async file => {
     const filePath = path.join(folder, file)
@@ -27,11 +28,11 @@ async function ls(folder) {
     const fileSize = stats.size.toString()
     const fileModified = stats.mtime.toLocaleString()
 
-    return `${fileType} ${file.padEnd(20)} ${fileSize.padStart(10)} ${fileModified} `
+    return `${pc.bgCyan(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)} `
   })
 
   const filesInfo = await Promise.all(filesPromises)
-  
+
   filesInfo.forEach(fileInfo => console.log(fileInfo))
 }
 
